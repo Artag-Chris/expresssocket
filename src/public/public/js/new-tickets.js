d3fd2c52cf762f7e4a8bcc5cd87479b09e6d2182
Artag-Chris <scristxyz@gmail.com>
2024-08-20 09:10:46 -0500

    chages

--- src/public/public/js/new-tickets.js
+++ src/public/public/js/new-tickets.js
@@ -5,7 +5,7 @@ const createTicketBtn = document.querySelector('button');
 
 
 async function getLastTicket(){
-    const lastTicket=await fetch("/api/ticket/last").then(res=>res.json());
+    const lastTicket=await fetch("localhost:3000/api/tickets/last").then(res=>res.json());
     currentTicketLBL.innerText=lastTicket;
  
     console.log(lastTicket);
